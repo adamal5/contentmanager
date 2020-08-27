232b180f83bc520666b73b242256007272a6d88e
Adama <akabba@qa.com>
2020-08-27 17:31:56 +0100

    Initial commit

---
+++ src/main/java/com/qa/contentmanager/ContentmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.qa.contentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/contentmanager/ContentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.contentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
