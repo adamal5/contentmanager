4b370a77139d6467ee6f380fe9f59760f0dc00c5
Adama <akabba@qa.com>
2020-08-27 18:59:56 +0100

    created repository for user input

---
+++ src/main/java/com/qa/contentmanager/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.qa.contentmanager.repo;
+
+import com.qa.contentmanager.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
