fbe7cbe52b8902341a7d0268847e2a10c21781ec
Adama <akabba@qa.com>
2020-08-27 18:57:51 +0100

    added user constructor, getters and setters

--- src/main/java/com/qa/contentmanager/domain/User.java
+++ src/main/java/com/qa/contentmanager/domain/User.java
@@ -28,4 +28,53 @@ public class User {
     @Column (nullable = false)
     private String password;
 
+    public User(){}
+
+    public User(String firstName, String lastName, String userName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
