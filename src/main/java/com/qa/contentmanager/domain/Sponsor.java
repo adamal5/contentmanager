353d8daf99191e8a55ec57ed1a3d7c07eb3678ee
Adama <akabba@qa.com>
2020-08-27 19:56:23 +0100

    Added constructor, getters and setters to sponsor

--- src/main/java/com/qa/contentmanager/domain/Sponsor.java
+++ src/main/java/com/qa/contentmanager/domain/Sponsor.java
@@ -30,6 +30,51 @@ public class Sponsor {
 
     public Sponsor(){}
 
+    public Sponsor(String companyName, String primaryContactName, String primaryContactEmail, String primaryContactPhone, String notes) {
+        this.companyName = companyName;
+        this.primaryContactName = primaryContactName;
+        this.primaryContactEmail = primaryContactEmail;
+        this.primaryContactPhone = primaryContactPhone;
+        this.notes = notes;
+    }
 
+    public String getCompanyName() {
+        return companyName;
+    }
 
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getPrimaryContactName() {
+        return primaryContactName;
+    }
+
+    public void setPrimaryContactName(String primaryContactName) {
+        this.primaryContactName = primaryContactName;
+    }
+
+    public String getPrimaryContactEmail() {
+        return primaryContactEmail;
+    }
+
+    public void setPrimaryContactEmail(String primaryContactEmail) {
+        this.primaryContactEmail = primaryContactEmail;
+    }
+
+    public String getPrimaryContactPhone() {
+        return primaryContactPhone;
+    }
+
+    public void setPrimaryContactPhone(String primaryContactPhone) {
+        this.primaryContactPhone = primaryContactPhone;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
