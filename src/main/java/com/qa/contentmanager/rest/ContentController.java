4112d9061913621ed81726493766521da75845a3
Adama <akabba@qa.com>
2020-08-29 14:19:17 +0100

    refactored content controller in line with DTO changes

--- src/main/java/com/qa/contentmanager/rest/ContentController.java
+++ src/main/java/com/qa/contentmanager/rest/ContentController.java
@@ -1,8 +1,11 @@
 package com.qa.contentmanager.rest;
 
 import com.qa.contentmanager.domain.Content;
+import com.qa.contentmanager.dto.ContentDTO;
 import com.qa.contentmanager.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,30 +22,32 @@ public class ContentController {
 
     //specifically used for read functionality/ browsers only able to work with getmapping
     @GetMapping("/")
-    public List<Content> getAllContent(){
-        return this.contentService.readAllContent();
+    public ResponseEntity<List<ContentDTO>> getAllContent() {
+        return ResponseEntity.ok(this.contentService.readAllContent());
     }
 
     // url for adding notes.. requestbody should include a note could be void depends on what you want to di with your abb
     @PostMapping("/createContent")
-    public Content createContent(@RequestBody Content content){
-        return this.contentService.createContent(content);
+    public ResponseEntity<ContentDTO> createContent(@RequestBody Content content) {
+        return new ResponseEntity<ContentDTO>(this.contentService.createContent(content), HttpStatus.CREATED);
     }
 
     //long name must be same as end of url e.g id here
     @DeleteMapping("/delete/{id}")
-    public Boolean deleteContent(@PathVariable Long id){
-        return this.contentService.deleteContentById(id);
+    public ResponseEntity<?> deleteContent(@PathVariable Long id){
+        return this.contentService.deleteContentById(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getContentById/{id}")
-    public Content getContentById(@PathVariable Long id){
-        return this.contentService.findContentById(id);
+    public ResponseEntity<ContentDTO> getContentById(@PathVariable Long id){
+        return ResponseEntity.ok(this.contentService.findContentById(id));
     }
 
     @PutMapping("/updateContent/{id}")
-    public Content updateContent(@PathVariable Long id, @RequestBody Content content){
-        return this.contentService.updateContent(id, content);
+    public ResponseEntity <ContentDTO> updateContent(@PathVariable Long id, @RequestBody Content content){
+        return ResponseEntity.ok(this.contentService.updateContent(id, content));
     }
 
 }
