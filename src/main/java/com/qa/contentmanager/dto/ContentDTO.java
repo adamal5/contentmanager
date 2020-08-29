f2c4d9d1168492c21a2ee7b2984aa5c450a1a888
Adama <akabba@qa.com>
2020-08-29 13:22:53 +0100

    added contentDTO functionality

--- src/main/java/com/qa/contentmanager/dto/ContentDTO.java
+++ src/main/java/com/qa/contentmanager/dto/ContentDTO.java
@@ -1,4 +1,89 @@
 package com.qa.contentmanager.dto;
 
+import com.qa.contentmanager.domain.Sponsor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class ContentDTO {
+
+
+    private Long contentID;
+    private String title;
+    private String contentType;
+    private String platform;
+    private String status;
+    private String postDate;
+    private String notes;
+    private Sponsor sponsor;
+
+    public ContentDTO() {
+    }
+
+    public ContentDTO(String title, String contentType, String platform, String status, String postDate, String sponsor, String notes) {
+        this.title = title;
+        this.contentType = contentType;
+        this.platform = platform;
+        this.status = status;
+        this.postDate = postDate;
+        this.notes = notes;
+    }
+
+    public Long getContentID() {
+        return contentID;
+    }
+
+    public void setContentID(Long contentID) {
+        this.contentID = contentID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getNotes() { return notes; }
+
+    public void setNotes(String notes) { this.notes = notes; }
+
+    public Sponsor getSponsor() { return sponsor; }
+
+    public void setSponsor(Sponsor sponsor) { this.sponsor = sponsor; }
 }
